d26df020fe0c4098cd67f29c7ba7237479cc6dfd
Abdelrahman Ibrahim <160507196+ENGibr0him@users.noreply.github.com>
2024-07-22 15:46:36 +0200

    adding arabic lang v.101

--- categories.js
+++ categories.js
@@ -1,12 +1,24 @@
-// categories.js
 const categories = {
-  movies: ["Brave Heart", "The Godfather", "Inception"],
-  books: ["To Kill a Mockingbird", "1984", "The Great Gatsby"],
-  quotes: [
-    "To be or not to be",
-    "I think therefore I am",
-    "The only thing we have to fear is fear itself",
-  ],
+  movies: {
+    English: ["Brave Heart", "The Godfather", "Inception"],
+    Arabic: ["قلب شجاع", "العراب", "التصاعد"],
+  },
+  books: {
+    English: ["To Kill a Mockingbird", "1984", "The Great Gatsby"],
+    Arabic: ["لقتل الطائر المحاكي", "١٩٨٤", "غاتسبي العظيم"],
+  },
+  quotes: {
+    English: [
+      "To be or not to be",
+      "I think therefore I am",
+      "The only thing we have to fear is fear itself",
+    ],
+    Arabic: [
+      "أن تكون أو لا تكون",
+      "أنا أفكر إذن أنا موجود",
+      "الشيء الوحيد الذي يجب أن نخافه هو الخوف نفسه",
+    ],
+  },
 };
 
 export default categories;
